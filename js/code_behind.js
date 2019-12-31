3eb66ac4d6ceb4822a6842995b367b939253da04
Sairam Madem <smadem@ptc.com>
2019-12-31 11:14:56 -0500

    A separate code behind js folder is created.

---
+++ js/code_behind.js
@@ -0,0 +1,14 @@
+function fn1() {
+  $("#heading1").fadeToggle(5000);
+  // $("#heading1").css("background-color", "yellow");
+  // $("#heading1").css("color", "yellow");
+  /* $("#heading1").animate(
+                {
+                    width: "300px",
+                    height: "300px",
+                    marginLeft: "150px",
+                    borderWidth: "10px",
+                    opacity: 0.5
+                }
+            ) */
+}

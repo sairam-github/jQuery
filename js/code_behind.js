bc4e0d5f29e4bdcc0e3a7bbabb6819a8fa40f247
Sairam Madem <smadem@ptc.com>
2019-12-31 11:44:53 -0500

    code behind is updated.

--- js/code_behind.js
+++ js/code_behind.js
@@ -1,14 +1,9 @@
-function fn1() {
+$(document).ready(on_ready);
+
+function on_ready() {
+    $('#btn1').on('click', btn_click);
+}
+
+function btn_click() {
   $("#heading1").fadeToggle(5000);
-  // $("#heading1").css("background-color", "yellow");
-  // $("#heading1").css("color", "yellow");
-  /* $("#heading1").animate(
-                {
-                    width: "300px",
-                    height: "300px",
-                    marginLeft: "150px",
-                    borderWidth: "10px",
-                    opacity: 0.5
-                }
-            ) */
 }

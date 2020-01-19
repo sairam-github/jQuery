581ee5ee3b80b878e4380bd671c7615f98fe4995
Sairam Madem <smadem@ptc.com>
2020-01-18 19:29:29 -0500

    jQuery effects - slideToggle, fadeToggle are added

--- jQuery_effects/js/code_behind.js
+++ jQuery_effects/js/code_behind.js
@@ -3,16 +3,24 @@ $(document).ready(on_ready);
 function on_ready() {
     $('#btn1').on('click', btn1_click);
     $('#btn2').on('click', btn2_click);
+    $('#btn3').on('click', btn3_click);
 }
 
 function btn1_click() {
     // $('#firstdiv').hide();
     //$('#firstdiv').toggle();
     // $('#firstdiv').toggle(2000);
-    $('#firstdiv').fadeOut(2000);
+    // $('#firstdiv').fadeOut(2000);
+    // $('#firstdiv').fadeToggle(2000);
+    $('#firstdiv').slideUp();
 }
 
 function btn2_click() {
     // $('#firstdiv').show();
-    $('#firstdiv').fadeIn(2000);
+    // $('#firstdiv').fadeIn(2000);
+    $('#firstdiv').slideDown();
+}
+
+function btn3_click() {
+    $('#firstdiv').slideToggle();
 }

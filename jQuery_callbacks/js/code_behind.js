776f0bc62eb7c812f22e6b076404cc2d7d196c21
Sairam Madem <smadem@ptc.com>
2020-01-18 20:27:21 -0500

    jQuery - callbacks

---
+++ jQuery_callbacks/js/code_behind.js
@@ -0,0 +1,11 @@
+$(document).ready(on_ready);
+
+function on_ready() {
+    $('#btnToggle').on('click', btnToggle_click);
+}
+
+function btnToggle_click() {
+    $('#firstdiv').fadeToggle(5000, function() {
+        alert("division is faded");
+    });
+}
